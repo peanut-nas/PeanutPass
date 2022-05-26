ae04c739bc372aacd4afcf64f9514dbece629bce
panrunqiu <panrunqiu@outlook.com>
2022-05-26 19:09:59 +0800

    优化 大图标复制密码功能

--- app/src/main/java/com/peanut/passwordmanager/ui/screens/home/LargeAccountItem.kt
+++ app/src/main/java/com/peanut/passwordmanager/ui/screens/home/LargeAccountItem.kt
@@ -2,8 +2,6 @@ package com.peanut.passwordmanager.ui.screens.home
 
 import android.widget.Toast
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
