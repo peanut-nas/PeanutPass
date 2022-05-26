9131d7b3874449cc2c43153542d82b081b381c2b
panrunqiu <panrunqiu@outlook.com>
2022-05-26 18:52:05 +0800

    优化 大图标复制密码功能

--- app/src/main/java/com/peanut/passwordmanager/ui/screens/home/LargeAccountItem.kt
+++ app/src/main/java/com/peanut/passwordmanager/ui/screens/home/LargeAccountItem.kt
@@ -1,14 +1,17 @@
 package com.peanut.passwordmanager.ui.screens.home
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.peanut.passwordmanager.R
 import com.peanut.passwordmanager.data.models.Account
 import com.peanut.passwordmanager.util.AccountType
 import com.peanut.passwordmanager.util.AdditionalFunctions.copy
@@ -16,8 +19,14 @@ import com.peanut.passwordmanager.util.AdditionalFunctions.copy
 @Composable
 fun LargeAccountItem(account: Account) {
     val context = LocalContext.current
-   AccountIcon(modifier = Modifier.clickable { account.password.copy(context) },
-       accountName = account.title, icon = account.icon, width = 60.dp, fontSize = 32.sp)
+    val message = stringResource(id = R.string.account_password_copy)
+    AccountIcon(
+        modifier = Modifier.clickable {
+            account.password.copy(context)
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        },
+        accountName = account.title, icon = account.icon, width = 60.dp, fontSize = 32.sp
+    )
 }
 
 @Composable
