84734044ee66c5a5882c2118b2c41ccfc2f682bc
panrunqiu <panrunqiu@outlook.com>
2022-05-26 14:37:51 +0800

    优化 列表与圆角

--- app/src/main/java/com/peanut/passwordmanager/ui/screens/home/AllAccountsContent.kt
+++ app/src/main/java/com/peanut/passwordmanager/ui/screens/home/AllAccountsContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -23,31 +24,33 @@ import kotlinx.coroutines.launch
 @SuppressLint("CoroutineCreationDuringComposition")
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun AllAccountsContent(allAccounts: List<Account>, navigateToItemScreen: (Int) -> Unit, sharedViewModel: SharedViewModel){
-    Surface(shape = MaterialTheme.shapes.medium.copy(CornerSize(12.dp))) {
-        LazyColumn {
-            items(items = allAccounts, key = { account: Account ->
-                account.id
-            }) { account ->
-                val dismissState = rememberDismissState()
-                val dismissDirection = dismissState.dismissDirection
-                val isDismissed = dismissState.isDismissed(DismissDirection.EndToStart)
-                if (isDismissed && dismissDirection == DismissDirection.EndToStart){
-                    val scope = rememberCoroutineScope()
-                    scope.launch {
-                        delay(300)
-                        sharedViewModel.action.value = Action.DELETE
-                        //更新被删除的值，方便再添加
-                        sharedViewModel.updateAccountFields(account)
-                    }
+fun AllAccountsContent(allAccounts: List<Account>, navigateToItemScreen: (Int) -> Unit, sharedViewModel: SharedViewModel) {
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(items = allAccounts, key = { account: Account ->
+            account.id
+        }) { account ->
+            val dismissState = rememberDismissState()
+            val dismissDirection = dismissState.dismissDirection
+            val isDismissed = dismissState.isDismissed(DismissDirection.EndToStart)
+            if (isDismissed && dismissDirection == DismissDirection.EndToStart) {
+                val scope = rememberCoroutineScope()
+                scope.launch {
+                    delay(300)
+                    sharedViewModel.action.value = Action.DELETE
+                    //更新被删除的值，方便再添加
+                    sharedViewModel.updateAccountFields(account)
                 }
-                val degrees by animateFloatAsState(targetValue = if (dismissState.targetValue == DismissValue.Default) 0f else -45f )
-                var itemAppeared by remember { mutableStateOf(false)}
-                LaunchedEffect(key1 = true, block = { itemAppeared = true })
-                AnimatedVisibility(visible = itemAppeared && !isDismissed, enter = expandVertically(animationSpec = tween(durationMillis = 300)),
-                    exit = shrinkVertically(animationSpec = tween(durationMillis = 300))) {
+            }
+            val degrees by animateFloatAsState(targetValue = if (dismissState.targetValue == DismissValue.Default) 0f else -45f)
+            var itemAppeared by remember { mutableStateOf(false) }
+            LaunchedEffect(key1 = true, block = { itemAppeared = true })
+            Surface(shape = MaterialTheme.shapes.medium.copy(CornerSize(12.dp))) {
+                AnimatedVisibility(
+                    visible = itemAppeared && !isDismissed, enter = expandVertically(animationSpec = tween(durationMillis = 300)),
+                    exit = shrinkVertically(animationSpec = tween(durationMillis = 300))
+                ) {
                     SwipeToDismiss(state = dismissState, directions = setOf(DismissDirection.EndToStart), dismissThresholds = { FractionalThreshold(0.2f) },
-                        background = { RedBackground(degrees = degrees)}) {
+                        background = { RedBackground(degrees = degrees) }) {
                         SmallAccountItem(account = account, navigateToItemScreen = navigateToItemScreen, sharedViewModel = sharedViewModel)
                     }
                 }
